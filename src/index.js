62f070ce0d859018ab93fe537c332e02baf64f08
Vlad <vvllaadd1233345@gmail.com>
2021-10-23 14:39:42 +0300

    update-work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import changeTheme from "./change-theme.js";
-import createItems from "./create-item.js";
+import changeTheme from "./js/change-theme.js";
+import createItems from "./js/create-item.js";
 
 changeTheme();
 createItems;
--- src/js/create-item.js
+++ src/js/create-item.js
@@ -1,5 +1,5 @@
-import compileHandle from "./handle-item.hbs";
-import baseForItem from "./menu.json";
+import compileHandle from "../handle-item.hbs";
+import baseForItem from "../menu.json";
 const listForItems = document.querySelector(".js-menu");
 
 const items = compileHandle(baseForItem);
